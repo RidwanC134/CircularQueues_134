9904e43e96782d10f23bf8582b76ecaad9284fd5
RidwanC134 <ridwanhh10@gmail.com>
2023-06-22 14:53:38 +0700

    Add project files.

---
+++ CircularQueues_134/CircularQueues_134.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
+public:
+	Queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << endl;
+
+		// Cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\Queue overflow\n";
+			return;
+		}
+
+		// Cek apakah antrian kosong
+	}
+};
